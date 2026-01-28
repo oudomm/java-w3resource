45a6fc65b5dadf0d7b4b39b2415b99d9dadf4e08
oudom <oudomphoem@gmail.com>
2026-01-28 12:06:23 +0700

    exercise5

---
+++ src/basic/part1/Exercise5.java
@@ -0,0 +1,14 @@
+package basic.part1;
+
+import java.util.Scanner;
+
+public class Exercise5 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + " x " + num2 + " = " + num2 * num2);
+    }
+}
