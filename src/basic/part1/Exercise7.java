708bcab6efef2d85c0a324ceb480dc384699b947
oudom <oudomphoem@gmail.com>
2026-01-28 12:18:13 +0700

    exercise7

---
+++ src/basic/part1/Exercise7.java
@@ -0,0 +1,15 @@
+package basic.part1;
+
+import java.util.Scanner;
+
+public class Exercise7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int num = scanner.nextInt();
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(num + " x " + i + " = " + (num * i));
+        }
+    }
+}
