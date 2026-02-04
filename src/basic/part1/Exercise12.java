1091c3c1d42b6c37d86b440af03b7b0f845a5728
oudom <oudomphoem@gmail.com>
2026-02-04 22:13:36 +0700

    exercise12

---
+++ src/basic/part1/Exercise12.java
@@ -0,0 +1,17 @@
+package basic.part1;
+
+import java.util.Scanner;
+
+public class Exercise12 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("num1 = ");
+        double num1 = scanner.nextDouble();
+        System.out.print("num2 = ");
+        double num2 = scanner.nextDouble();
+        System.out.print("num3 = ");
+        double num3 = scanner.nextDouble();
+        double average = (num1 + num2 + num3) / 3;
+        System.out.println("Average = " + average);
+    }
+}
