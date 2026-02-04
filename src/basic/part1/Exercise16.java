ede7475bf33e3debeb0f5e8ed8712bef1f54bba6
oudom <oudomphoem@gmail.com>
2026-02-04 22:30:31 +0700

    exercise16

---
+++ src/basic/part1/Exercise16.java
@@ -0,0 +1,11 @@
+package basic.part1;
+
+public class Exercise16 {
+    public static void main(String[] args) {
+        System.out.println(" +\"\"\"\"\"+");
+        System.out.println("[| o o |]");
+        System.out.println(" |  ^  |");
+        System.out.println(" | '-' |");
+        System.out.println(" +-----+");
+    }
+}
